00545474d785f88a27f4c0f0b9a5d78c8663ecc3
Atul Jangra <atuljangra66@gmail.com>
2021-05-01 17:13:45 -0400

    Add ability to check vaccine appointment availability in different centers
    As title.

--- parser.py
+++ parser.py
@@ -17,22 +17,38 @@ def get_district_data():
             print(f"Error{ex}: state_id:{state_id}")
             pass
 
-def get_apts_by_pin(pin_code, num_days=7):
+
+def capacity_available_in_center(center_data, query_date):
+    # Get sessions
+    try:
+        sessions = center_data["sessions"]
+        for session in sessions:
+            is_available = session["available_capacity"] > 0
+            if is_available:
+                print("Vaccine appointment available at ", center_data['name'], " on:", query_date)
+    except:
+        print("Error fetching sessions")
+
+
+def get_apts_by_pin(pin_code=100, num_days=1):
     today = datetime.datetime.today()
     date_list = [today + datetime.timedelta(days=x) for x in range(num_days)]
     date_str = [x.strftime("%d-%m-%Y") for x in date_list]
     for query_date in date_str:
         req_str = f"https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={pin_code}&date={query_date}"
-        print(req_str)
         response = requests.get(req_str)
+        print("Checking for ", query_date)
         if response.ok:
             response_json = response.json()
             if (response_json["centers"]):
-                print(f"You can book vaccine slots on {query_date}")
+                # Check if appointment is actually available.
+                center_id_lists = response_json["centers"]
+                for center in center_id_lists:
+                    is_capacity_available_in_center(center, query_date)
                 ## TODO: Take a flag and show exact appointments here.
                 ## Might not be too useful as you need to go to cowin to book anyway.
             else:
-                print(f"No appointment available on {query_date}")
+                print(f"No centers available on {query_date}")
         else:
             print("Covin server seems down.")
 
@@ -43,7 +59,6 @@ def parse_and_fetch():
     pin_code = input()
     print("Enter the number of days in future, default 7:")
     num_days = int(input())
-    # print(pin_code, num_days)
     get_apts_by_pin(pin_code, num_days)
     print("done")
 
